4209426571f1bce7b6230621c2753f53e63237be
Stephon <stidd@mail.valenciacollege.edu>
2023-07-27 00:13:37 -0400

    Made some directory changes

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ app = Flask(__name__)
 #decerator used to map function to a particular part of url
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")#finds the html given and displays to user
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True) #stops the need to close server
