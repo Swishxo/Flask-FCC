8f79a2f4ac2eddc88b9477d2a78267e7284ca3a6
Stephon <stidd@mail.valenciacollege.edu>
2023-07-27 11:17:45 -0400

    Updated

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-#imports needed for flask
 from flask import Flask, render_template, request
 
-#passed the name of the module or package of the application
 app = Flask(__name__)
 
-#decerator used to map function to a particular part of url
+#easier way to get data from HTTP
 @app.route("/")
 def index():
-    if "name" in request.args:#if there is a name in request.args
-        name = request.args["name"]#get name from HTTP and store in var (name)
-    else:
-        name = "World" #if no HTTP (name) provided. 
-        #'render_template()' also takes named args of your choice to pass to template dir files
-    return render_template("index.html", placeholder=name)#finds the html given and displays to user
+    return render_template("index.html")
 
 
 
@@ -22,4 +15,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True) #stops the need to close server
+    app.run(debug=True)
