451b3b0b3a8f60ae2f83f2dfb37263e2ec02f858
Stephon <stidd@mail.valenciacollege.edu>
2023-07-27 00:00:14 -0400

    added comments

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+#imports needed for flask
 from flask import Flask, render_template, request
 
+#passed the name of the module or package of the application
 app = Flask(__name__)
 
+#decerator used to map function to a particular part of url
 @app.route("/")
 def index():
     return "Hello, World"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) #stops the need to close server
